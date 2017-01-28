4d71b25d8e0076bee8a6e34a5f489d77641bc8d0
adamnew123456 <adamnew123456@gmail.com>
2017-01-28 16:20:11 -0500

    Add ability to launch supervisor without daemonizing
    This makes tests much easier to write, becuase they don't have to worry
    about killing processes that might have gone awry - instead, they can kill
    them like normal children.
    
    Also, this fix utilizes the logging reset added in the previous commit.

--- jobmon/launcher.py
+++ jobmon/launcher.py
@@ -15,7 +15,7 @@ import os
 import sys
 
 from jobmon import (
-    daemon, service, command_server, event_server, status_server, ticker
+    daemon, service, command_server, event_server, status_server, ticker, util
 )
 
 # Make sure that we get console logging before the supervisor becomes a
@@ -25,7 +25,7 @@ logging.basicConfig(level=logging.INFO,
 
 LOGGER = logging.getLogger('jobmon.launcher')
 
-def run(config_handler, as_daemon=True):
+def run_daemon(config_handler, as_daemon=True):
     """
     Starts the supervisor daemon, passing to it the appropriate 
     configuration.
@@ -47,49 +47,81 @@ def run(config_handler, as_daemon=True):
 
     supervisor_wrapper.start(config_handler)
 
+def run_fork(config_handler):
+    """
+    Starts the supervisor as a direct child process, passing to it the appropriate 
+    configuration. This is meant for use during tests, when the child process needs
+    to be monitored (and possibly killed if it crashes) instead of allowed to 
+    roam free as in the daemon case.
+
+    :param config.ConfigHandler config_handler: The configuration to run the \
+    supervisor with.
+    :return int: The PID of the child process that was launched.
+    """
+    logging.info('Sending log messages[%s] to %s', 
+            config_handler.log_level,
+            config_handler.log_file)
+
+    pid = os.fork()
+    if pid == 0:
+        LOGGER.info('In child: starting processing')
+        execute_supervisor(config_handler)
+    else:
+        return pid
+
+def execute_supervisor(config_handler):
+    """
+    Runs the supervisor according to the given configuration.
+
+    :param config.ConfigHandler config_handler: The configuration.
+    """
+    # Read the jobs and start up the supervisor, and then make sure to
+    # die if we exit
+    try:
+        util.reset_loggers()
+        logging.basicConfig(filename=config_handler.log_file, 
+                            level=config_handler.log_level,
+                            format='%(name)s %(asctime)s %(message)s')
+
+
+        supervisor_shim = service.SupervisorShim()
+        events = event_server.EventServer(config_handler.event_port)
+
+        restart_svr = ticker.Ticker(supervisor_shim.on_job_timer_expire)
+        commands = command_server.CommandServer(
+            config_handler.control_port, supervisor_shim)
+
+        status = status_server.StatusServer(supervisor_shim)
+
+        supervisor = service.SupervisorService(
+                config_handler, events, status, restart_svr)
+
+        events.start()
+        commands.start()
+        status.start()
+        restart_svr.start()
+        supervisor.start()
+
+        # This has to be done last, since it starts up the autostart
+        # jobs and gets the ball rolling
+        supervisor_shim.set_service(supervisor)
+
+        # The event server should be the last to terminate, since it
+        # has to tell the outside world that we're gone
+        LOGGER.info('Waiting for events to exit')
+        events.wait_for_exit()
+    except Exception as ex:
+        LOGGER.error('DEAD SUPERVISOR', exc_info=True)
+    finally:
+        LOGGER.info('Peace out!')
+        os._exit(0)
+
 class SupervisorDaemon(daemon.Daemon):
     def run(self, config_handler):
         """
         Runs the supervisor according to the given configuration.
 
-        :param service.Supervisor supervisor: The supervisor to run.
         :param config.ConfigHandler config_handler: The configuration.
         """
-        logging.basicConfig(filename=config_handler.log_file, 
-                            level=config_handler.log_level,
-                            format='%(asctime)s %(message)s')
-
-        # Read the jobs and start up the supervisor, and then make sure to
-        # die if we exit
-        try:
-            supervisor_shim = service.SupervisorShim()
-            events = event_server.EventServer(config_handler.event_port)
-
-            restart_svr = ticker.Ticker(supervisor_shim.on_job_timer_expire)
-            commands = command_server.CommandServer(
-                config_handler.control_port, supervisor_shim)
-
-            status = status_server.StatusServer(supervisor_shim)
-
-            supervisor = service.SupervisorService(
-                    config_handler, events, status, restart_svr)
-
-            events.start()
-            commands.start()
-            status.start()
-            restart_svr.start()
-            supervisor.start()
-
-            # This has to be done last, since it starts up the autostart
-            # jobs and gets the ball rolling
-            supervisor_shim.set_service(supervisor)
-
-            # The event server should be the last to terminate, since it
-            # has to tell the outside world that we're gone
-            LOGGER.info('Waiting for events to exit')
-            events.wait_for_exit()
-        except Exception as ex:
-            LOGGER.error('DEAD SUPERVISOR', exc_info=True)
-        finally:
-            LOGGER.info('Peace out!')
-            sys.exit(0)
+        LOGGER.info('Done daemonizing, launching supervisor')
+        execute_supervisor(config_handler)
